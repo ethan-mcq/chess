2d3709fce97968a2f4fe42c877fa8717267b7588
ethanmcquhae <ethan.mcq01@gmail.com>
2024-09-10 09:03:32 -0600

    adding other classes

---
+++ shared/src/main/java/chess/KingMoves.java
@@ -0,0 +1,15 @@
+package chess;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class KingMoves implements PieceMoves {
+
+    @Override
+    public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        // Implement here pls
+
+        return moves;
+    }
+
+}
