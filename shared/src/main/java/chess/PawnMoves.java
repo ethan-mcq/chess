c5d0ad5f5848665b4b793c5359c500166f823f3b
ethanmcquhae <ethan.mcq01@gmail.com>
2024-09-17 19:50:07 -0600

    some tests

--- shared/src/main/java/chess/PawnMoves.java
+++ shared/src/main/java/chess/PawnMoves.java
@@ -30,6 +30,17 @@ public class PawnMoves implements PieceMoves {
         int row = position.getRow();
         int col = position.getColumn();
         int moveRow = row + colorDirectionAssignment; // we only move rows
+
+        if (moveRow > 0 && moveRow < boardWidthLen && board.getPiece(new ChessPosition(moveRow, col)) == null) { // only move rows and cant move if piece is in front
+            promotionMoves(position, new ChessPosition(moveRow, col), moves, colorDirectionAssignment);
+
+            // Is it first move
+            int startPosition = colorDirectionAssignment == 1 ? 2 : 7;
+            moveRow = row + 2 * colorDirectionAssignment;
+            if (moveRow == startPosition && board.getPiece(new ChessPosition(moveRow, col)) == null) {
+                moves.add(new ChessMove(position, new ChessPosition(moveRow, col), null));
+            }
+        }
     }
     private void captureMoves(ChessBoard board, ChessPosition position, Collection<ChessMove> moves, int colorDirectionAssignment) {
         int boardWidthLen = 9;
@@ -38,13 +49,15 @@ public class PawnMoves implements PieceMoves {
         int moveRow = row + colorDirectionAssignment;
         int[] captureCols = { col + 1, col -1 }; // we can capture diagonally on either side
     }
-    private void promotionMoves(ChessBoard board, ChessPosition position, Collection<ChessMove> moves, int colorDirectionAssignment) {
+    private void promotionMoves(ChessPosition position, ChessPosition newPosition, Collection<ChessMove> moves, int colorDirectionAssignment) {
         int promoRow = colorDirectionAssignment == 1 ? 8 : 1; // if its white then it will be in row 8, 1 if black
         if (position.getRow() == promoRow) {
             moves.add(new ChessMove(position, null, ChessPiece.PieceType.QUEEN));
             moves.add(new ChessMove(position, null, ChessPiece.PieceType.KNIGHT));
             moves.add(new ChessMove(position, null, ChessPiece.PieceType.ROOK));
             moves.add(new ChessMove(position, null, ChessPiece.PieceType.BISHOP));
+        } else {
+            moves.add(new ChessMove(position, newPosition, null));
         }
     }
 }
