1fdba1a5a0cde03f05b0706a9d74afbdd7ece06c
ethanmcquhae <ethan.mcq01@gmail.com>
2024-11-18 11:20:56 -0700

    update3

--- server/src/test/java/service/AuthTest.java
+++ server/src/test/java/service/AuthTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+//fixed
 public class AuthTest {
 
     private Data dataSource;
